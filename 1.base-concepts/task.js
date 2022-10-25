253c7e3580da27dd56c297fbcad083f8955d9874
Boolgakova <boolgakova@gmail.com>
2022-10-25 18:41:50 +0300

    Base-concepts task fixed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,39 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = Math.pow(b,2)-4*a*c;
-  let x1 = (-b + Math.sqrt(d))/(2*a);
-  let x2 = (-b - Math.sqrt(d))/(2*a);
+  const array = [];
+  const discriminant = Math.pow(b,2)-4*a*c;
+  let x1 = (-b + Math.sqrt(discriminant))/(2*a);
+  let x2 = (-b - Math.sqrt(discriminant))/(2*a);
   let x3 = -b/(2*a);
-  if (d > 0){
-  arr.push(x1);
-  arr.push(x2);
-  } else if (d = 0){
-  arr.push(x3);
+  if (discriminant > 0){
+  array.push(x1);
+  array.push(x2);
+  } else if (discriminant == 0){
+  array.push(x3);
   }
-  return arr;
+  return array;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  if (isNaN(percent)){
+  return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
+
+  if (isNaN(contribution)){
+  return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+
+  if (isNaN(amount)){
+  return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+
   let totalAmount;
-  let creditBody = amount - contribution;
+  const creditBody = amount - contribution;
   let now = new Date();
   let term = date.getMonth() - now.getMonth() +
                 (12 * (date.getFullYear() - now.getFullYear()));
-  let monthlyRate = percent /(100 * 12);
+  const monthlyRate = percent /(100 * 12);
   let monthlyPayment = creditBody * (monthlyRate + (monthlyRate / (((1 + monthlyRate)**term) - 1)));
   totalAmount = Number((monthlyPayment*term).toFixed(2));
   return totalAmount;
